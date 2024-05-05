08267336c431a58ee77eb255d814a43a2fa91f6c
Ubuntu <ubuntu@ip-10-240-184-225.us-west-2.compute.internal>
2024-05-05 00:53:37 +0000

    Update app.py to include ImaginativeComplimentEngine

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from engines.simple_compliment_engine import SimpleComplimentEngine
 from engines.feature_compliment_engine import FeatureComplimentEngine
+from engines.imaginative_compliment_engine import ImaginativeComplimentEngine
 import random
 
 app = Flask(__name__)
@@ -8,7 +9,7 @@ app = Flask(__name__)
 @app.route('/compliment', methods=['GET'])
 def generate_compliment():
     # Randomly select a compliment engine
-    engine_class = random.choice([SimpleComplimentEngine, FeatureComplimentEngine])
+    engine_class = random.choice([SimpleComplimentEngine, FeatureComplimentEngine, ImaginativeComplimentEngine])
     engine = engine_class()
     compliment = engine.generate_compliment()
     return jsonify({'compliment': compliment})
