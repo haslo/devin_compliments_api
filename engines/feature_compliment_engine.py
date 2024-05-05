c17b05f0c8f1cf4c073632847a37f6d113008254
🤖 Devin <ubuntu@ip-10-240-184-225.us-west-2.compute.internal>
2024-05-05 18:51:05 +0000

    Refactor FeatureComplimentEngine to use external YAML configuration

--- engines/feature_compliment_engine.py
+++ engines/feature_compliment_engine.py
@@ -1,25 +1,12 @@
 import random
 from dictionary_loader import DictionaryLoader
 
-# Load dictionaries from YAML file
-dictionary_loader = DictionaryLoader('compliment_dictionaries.yaml')
-dictionaries = dictionary_loader.load_dictionaries()
-
 class FeatureComplimentEngine:
     def __init__(self):
         self.template = "Your {feature} is {adjective}."
-        self.components = {
-            'feature': 'features',
-            'adjective': 'adjectives'
-        }
-        self.id = "feature"
-        # Define contextually appropriate feature-adjective pairs
-        self.acceptable_pairs = [
-            ('smile', 'radiant'),
-            ('style', 'impeccable'),
-            ('sense of humor', 'infectious'),
-            # ... more pairs can be added here
-        ]
+        dictionary_loader = DictionaryLoader('compliment_dictionaries.yaml')
+        self.dictionaries = dictionary_loader.load_dictionaries()
+        self.acceptable_pairs = self.dictionaries['feature_adjective_pairs']
 
     def generate_compliment(self):
         # Select a random contextually appropriate pair
@@ -27,4 +14,4 @@ class FeatureComplimentEngine:
         compliment = self.template.format(feature=feature, adjective=adjective)
         # Capitalize the first letter of the compliment
         compliment = compliment[0].upper() + compliment[1:]
-        return f"{compliment} [{self.id}]."
+        return compliment
