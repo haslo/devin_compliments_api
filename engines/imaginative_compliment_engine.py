e0d4805802e7dd120fae1bc77a5841264cb8b88a
Ubuntu <ubuntu@ip-10-240-184-225.us-west-2.compute.internal>
2024-05-05 00:51:13 +0000

    Implement ImaginativeComplimentEngine class

---
+++ engines/imaginative_compliment_engine.py
@@ -0,0 +1,19 @@
+import random
+from dictionary_loader import DictionaryLoader
+
+# Load dictionaries from YAML file
+dictionary_loader = DictionaryLoader('compliment_dictionaries.yaml')
+dictionaries = dictionary_loader.load_dictionaries()
+
+class ImaginativeComplimentEngine:
+    def __init__(self):
+        self.template = "You're {comparative} than {imaginary_thing}, because you're {presence}."
+        self.components = {
+            'comparative': 'comparatives',
+            'imaginary_thing': 'imaginary_things',
+            'presence': 'presences'
+        }
+
+    def generate_compliment(self):
+        compliment = self.template.format(**{k: random.choice(dictionaries[v]) for k, v in self.components.items()})
+        return compliment
