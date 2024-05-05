9513bed1927fde41d7895881fae7f4d5488f046e
Ubuntu <ubuntu@ip-10-240-184-225.us-west-2.compute.internal>
2024-05-05 00:44:40 +0000

    Implement CreativeComplimentEngine class

---
+++ engines/creative_compliment_engine.py
@@ -0,0 +1,20 @@
+import random
+from dictionary_loader import DictionaryLoader
+
+# Load dictionaries from YAML file
+dictionary_loader = DictionaryLoader('compliment_dictionaries.yaml')
+dictionaries = dictionary_loader.load_dictionaries()
+
+class CreativeComplimentEngine:
+    def __init__(self):
+        self.template = "You have the {adjective1} {noun1} of a {adjective2} {noun2}."
+        self.components = {
+            'adjective1': 'adjectives',
+            'noun1': 'nouns',
+            'adjective2': 'adjectives',
+            'noun2': 'nouns'
+        }
+
+    def generate_compliment(self):
+        compliment = self.template.format(**{k: random.choice(dictionaries[v]) for k, v in self.components.items()})
+        return compliment
