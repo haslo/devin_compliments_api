9dbdd16efd444281f0cdb38d2e7aeb3defdba066
Ubuntu <ubuntu@ip-10-240-195-108.us-west-2.compute.internal>
2024-05-07 11:30:44 +0000

    Organize example generation scripts into generate_examples folder

---
+++ generate_examples/generate_inclusive_examples.py
@@ -0,0 +1,13 @@
+from engines.inclusive_compliment_engine import InclusiveComplimentEngine
+from dictionary_loader import DictionaryLoader  # Corrected import statement
+
+# Load dictionaries
+dictionary_loader = DictionaryLoader('compliment_dictionaries.yaml')
+dictionaries = dictionary_loader.load_dictionaries()
+
+# Initialize engine
+engine = InclusiveComplimentEngine(dictionaries)
+
+# Generate examples
+for _ in range(10):
+    print(engine.generate_compliment())
