2cb4f3c54f041fb30ffc31267514034d608f1819
Ubuntu <ubuntu@ip-10-240-195-108.us-west-2.compute.internal>
2024-05-07 11:52:00 +0000

    Update InclusiveComplimentEngine for grammatical accuracy and add corresponding tests

--- engines/inclusive_compliment_engine.py
+++ engines/inclusive_compliment_engine.py
@@ -11,8 +11,12 @@ class InclusiveComplimentEngine:
             adjective=choice(self.dictionaries['inclusive_adjectives']),
             noun=choice(self.dictionaries['inclusive_nouns']),
             activity=choice(self.dictionaries['inclusive_activities']),
+            admirable_activity=choice(self.dictionaries['admirable_activities']),  # Added admirable_activities
             quality=choice(self.dictionaries['inclusive_qualities'])
         )
         # Capitalize the first letter of the compliment
         compliment = compliment[0].upper() + compliment[1:]
+        # Ensure the compliment ends with a period
+        if not compliment.endswith('.'):
+            compliment += '.'
         return compliment
