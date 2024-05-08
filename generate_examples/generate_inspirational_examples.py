c3c34ae8d87ac8de7756f6537caec3713768b1d8
🤖 Devin <devin@example.com>
2024-05-08 13:00:33 +0000

    Apply absolute import fix to generate_inspirational_examples.py

--- generate_examples/generate_inspirational_examples.py
+++ generate_examples/generate_inspirational_examples.py
@@ -1,11 +1,13 @@
 import sys
 sys.path.append('/home/ubuntu/devin_compliments_api')  # Add project directory to Python path
 
+# Updated import statements to use absolute imports
 from engines.inspirational_compliment_engine import InspirationalComplimentEngine
-from util.dictionary_loader import DictionaryLoader  # Updated import statement to reflect new util directory
+from util.dictionary_loader import DictionaryLoader
 
 # Load dictionaries
-dictionary_loader = DictionaryLoader('/home/ubuntu/devin_compliments_api/util/compliment_dictionaries.yaml')
+# Updated the path to use a relative path
+dictionary_loader = DictionaryLoader('util/compliment_dictionaries.yaml')
 dictionaries = dictionary_loader.load_dictionaries()
 
 # Create an instance of InspirationalComplimentEngine
