6f706edfdd632d10a9893836097c076f780486b6
Ubuntu <ubuntu@ip-10-240-177-131.us-west-2.compute.internal>
2024-05-08 07:48:24 +0000

    Add generator script for InspirationalComplimentEngine

---
+++ generate_examples/generate_inspirational_examples.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.append('/home/ubuntu/devin_compliments_api')  # Add project directory to Python path
+
+from engines.inspirational_compliment_engine import InspirationalComplimentEngine
+from util.dictionary_loader import DictionaryLoader  # Updated import statement to reflect new util directory
+
+# Load dictionaries
+dictionary_loader = DictionaryLoader('/home/ubuntu/devin_compliments_api/util/compliment_dictionaries.yaml')
+dictionaries = dictionary_loader.load_dictionaries()
+
+# Create an instance of InspirationalComplimentEngine
+engine = InspirationalComplimentEngine(dictionaries)
+
+# Generate and print a set of example compliments
+for _ in range(30):
+    print(engine.generate_compliment())
